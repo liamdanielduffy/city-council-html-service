17b1d978d4672e28bd822bde1e65510519af8675
Liam Duffy <liamdanielduffy@gmail.com>
2023-12-04 20:03:42 -0500

    adds support for clicking page btns that are not yet visible

--- src/city_council_html_service/core.clj
+++ src/city_council_html_service/core.clj
@@ -1,6 +1,5 @@
 (ns city-council-html-service.core
-  (:require [etaoin.api :as e]
-            [etaoin.keys :as k]))
+  (:require [etaoin.api :as e]))
 
 (defonce driver (e/chrome))
 
@@ -14,7 +13,7 @@
            })
 
 (def regex {
-            :page-state-text #"Page 1 of (\d+)"
+            :page-state-text #"Page \d+ of (\d+)"
             })
 
 (defn go-to [url]
@@ -23,35 +22,33 @@
 (defn query [xpath]
   (e/query driver xpath))
 
-(defn visit-calendar []
+(defn go-to-calendar []
   (go-to (:city-council-calendar url)))
+
+(defn click [el]
+  (e/click-el driver el))
 
 (defn get-children-of [element query]
   (e/children driver element query))
 
-(defn get-el-id [xpath-key]
+(defn query-el [xpath-key]
   (query (xpath-key xpath)))
 
-(defn get-page-buttons []
+(defn page-btns []
   (let
-    [page-buttons-container (get-el-id :page-buttons-container)
-     page-buttons (get-children-of
-                          page-buttons-container
-                          {:tag :a})]
-    page-buttons))
+    [btns-container (query-el :page-buttons-container)
+     btns (get-children-of btns-container {:tag :a})]
+    btns))
 
 (defn get-text-for-element [el-id]
   (e/get-element-text-el driver el-id))
 
-(defn get-page-button-labels []
-  (map get-text-for-element (get-page-buttons)))
+(defn page-btn-labels []
+  (map get-text-for-element (page-btns)))
 
-(defn get-page-buttons-by-label []
-  (zipmap (get-page-button-labels) (get-page-buttons)))
-
-(defn get-total-pages []
+(defn total-pages []
   (let
-    [el-id (get-el-id :page-state-text)
+    [el-id (query-el :page-state-text)
      text (get-text-for-element el-id)
      matched-text (re-find (:page-state-text regex) text)
      total-num-pages-str (nth matched-text 1)
@@ -59,29 +56,43 @@
     total-num-pages))
 
 (defn page-exists [page-num]
-  (<= page-num (get-total-pages)))
+  (<= page-num (total-pages)))
 
 (defn is-int-string [s]
   (try
     (do (Integer/parseInt s) true)
     (catch NumberFormatException _ false)))
 
-(defn get-visible-page-button-numbers []
-  (let [button-labels (get-page-button-labels)
-        button-labels-with-numbers (filter #(is-int-string %1) button-labels)
-        button-label-numbers (map #(Integer/parseInt %1) button-labels-with-numbers)]
-    button-label-numbers))
+(defn page-btn-numbers []
+  (let [btn-labels (page-btn-labels)
+        numbered-btn-labels (filter #(is-int-string %1) btn-labels)
+        numbers (map #(Integer/parseInt %1) numbered-btn-labels)]
+    numbers))
 
-(defn page-button-is-visible [page-num]
-  (let [
-        button-label-numbers (get-visible-page-button-numbers)
-        label-numbers-set (set button-label-numbers)]
-   (contains? label-numbers-set page-num)))
+(defn page-btn-is-visible [page-num]
+  (let [visible-numbers (page-btn-numbers)
+        is-visible (contains? (set visible-numbers) page-num)]
+    is-visible))
 
-(defn click-page-button [page-num]
-  (when (page-button-is-visible page-num)
-    (let [page-button-label (str page-num)
-          page-buttons-by-label (get-page-buttons-by-label)
-          page-button (page-buttons-by-label page-button-label)]
-      (e/click-el driver page-button))
-    ))
+(defn page-btns-by-label []
+  (zipmap (page-btn-labels) (page-btns)))
+
+(defn more-pages-btn []
+  ((page-btns-by-label) "..."))
+
+(defn wait-for-page-load []
+  (e/wait-visible driver (:page-buttons-container xpath)))
+
+(defn click-page-btn [page-num]
+  (let [is-visible (page-btn-is-visible page-num)
+        exists (page-exists page-num)
+        label (str page-num)
+        btn ((page-btns-by-label) label)]
+    (if is-visible
+      (do
+        (click btn)
+        (wait-for-page-load))
+      (when exists
+        (click (more-pages-btn))
+        (wait-for-page-load)
+        (recur page-num)))))
